06989b64a03c76cde54abf9815fd2326abbaa4d2
SularGaming <38816215+SularGaming@users.noreply.github.com>
2018-09-02 23:31:39 -0500

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,39 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const config = require("./config.json");
+
+client.on('ready', () => {
+  client.user.setGame('Follow me on Twitch! | t! for the Prefix! IN MAINTENANCE MODE!!!', "https://twitch.tv/toastwastaken");
+  console.log('Hello');
+});
+  
+client.on("message", async message => {
+  if(message.author.bot) return;
+  if(message.content.indexOf(config.prefix) !== 0) return;
+  
+const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+const command = args.shift().toLowerCase();
+
+if(command === "info") {
+    var embed = new Discord.RichEmbed()
+      .setThumbnail(message.client.avatarURL)
+      .addField("Hello my name is ToastyCord.", true)
+      .addField("I am developed to help those who cannot help themselves.", true)
+      .addField("Do t!help for more commands.")
+      .addField("Thanks for inviting me and enjoy.")
+      .setColor(0x00FFFF)
+    message.channel.sendEmbed(embed);
+  }
+
+//  /n == New line
+//  you can even do message.channel.sendMessage("```Hello```");
+
+});
+
+setInterval(() => {
+  http.get('http://discordjs-heroku.herokuapp.com');
+}, 900000);
+
+const port = process.env.PORT || 5000;
+
+client.login(process.env.BOT_TOKEN);
